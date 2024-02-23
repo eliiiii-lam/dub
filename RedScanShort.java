7b4c64a578ef497cf6e65ce1f05897331f97541b
ELIJAHH <144183598+eliiiii-lam@users.noreply.github.com>
2024-02-22 17:01:05 -0800

    Add files via upload

---
+++ RedScanShort.java
@@ -0,0 +1,305 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import com.acmerobotics.roadrunner.trajectory.TrajectoryBuilder;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.IMU;
+import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import com.qualcomm.robotcore.util.Range;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.Trajectory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+
+
+@Autonomous (name = "RedScanShort", group = "AUTO")
+public class RedScanShort extends LinearOpMode {
+
+    private DcMotor fl = null;
+    private DcMotor fr = null;
+    private DcMotor bl = null;
+    private DcMotor br = null;
+
+    DcMotor elbow;
+    DcMotor elbow2;
+
+
+    Servo clawL;
+    Servo clawR;
+
+    Servo wrist;
+
+    OpenCvCamera webcam;
+
+    private IMU imu = null;
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private double          headingError  = 0;
+
+    // These variable are declared here (as class members) so they can be updated in various methods,
+    // but still be displayed by sendTelemetry()
+    private double  targetHeading = 0;
+    private double  driveSpeed    = 0;
+    private double  turnSpeed     = 0;
+    private double  flSpeed= 0;
+    private double  frSpeed= 0;
+    private double  blSpeed= 0;
+    private double  brSpeed= 0;
+
+    private int     flTarget = 0;
+    private int     frTarget = 0;
+    private int     blTarget = 0;
+    private int     brTarget = 0;
+
+    // Calculate the COUNTS_PER_INCH for your specific drive train.
+    // Go to your motor vendor website to determine your motor's COUNTS_PER_MOTOR_REV
+    // For external drive gearing, set DRIVE_GEAR_REDUCTION as needed.
+    // For example, use a value of 2.0 for a 12-tooth spur gear driving a 24-tooth spur gear.
+    // This is gearing DOWN for less speed and more torque.
+    // For gearing UP, use a gear ratio less than 1.0. Note this will affect the direction of wheel rotation.
+    static final double COUNTS_PER_MOTOR_REV = 537.7;    // eg: TETRIX Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 1.0;     // No External Gearing.
+    static final double WHEEL_DIAMETER_INCHES = 3.77952;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+
+    static final double     DRIVE_SPEED             = 0.4;     // Max driving speed for better distance accuracy.
+    static final double     TURN_SPEED              = 0.4;     // Max Turn speed to limit turn rate
+    static final double     HEADING_THRESHOLD       = 0.5 ;    // How close must the heading get to the target before moving to next step.
+    // Requiring more accuracy (a smaller number) will often make the turn take longer to get into the final position.
+    // Define the Proportional control coefficient (or GAIN) for "heading control".
+    // We define one value when Turning (larger errors), and the other is used when Driving straight (smaller errors).
+    // Increase these numbers if the heading does not corrects strongly enough (eg: a heavy robot or using tracks)
+    // Decrease these numbers if the heading does not settle on the correct value (eg: very agile robot with omni wheels)
+    static final double     P_TURN_GAIN            = 0.02;     // Larger is more responsive, but also less stable
+    static final double     P_DRIVE_GAIN           = 0.03;     // Larger is more responsive, but also less stable
+
+
+
+
+    // Define motors and servos
+
+
+
+
+    RedSightPipeline pipeline = new RedSightPipeline(telemetry);
+
+    @Override
+    public void runOpMode() {
+        //initialize robot hardware
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        Pose2d startPose = new Pose2d(0,0,0);
+
+        fr = hardwareMap.get(DcMotor.class, "frontEncoder");
+        bl = hardwareMap.get(DcMotor.class, "leftEncoder");
+        br = hardwareMap.get(DcMotor.class, "rightEncoder");
+        fl = hardwareMap.get(DcMotor.class, "fL");
+
+
+        elbow = hardwareMap.dcMotor.get("elbow");
+        elbow2 = hardwareMap.dcMotor.get("elbow2");
+
+        wrist = hardwareMap.servo.get("wrist");
+
+        clawL = hardwareMap.servo.get("clawL");
+        clawR = hardwareMap.servo.get("clawR");
+
+
+
+
+
+        clawL.setPosition(0.4);
+        clawR.setPosition(0);
+
+
+
+
+
+        elbow.setDirection(DcMotor.Direction.REVERSE);
+        elbow.setDirection(DcMotorSimple.Direction.FORWARD);
+
+
+
+        // Now initialize the IMU with this mounting orientation
+        // This sample expects the IMU to be in a REV Hub and named "imu".
+
+
+
+        elbow.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        elbow2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        elbow.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        elbow2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        elbow.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        elbow2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        webcam = OpenCvCameraFactory.getInstance().createWebcam(hardwareMap.get(WebcamName.class, "Webcam 1"), cameraMonitorViewId);
+
+
+
+
+
+
+
+
+
+
+        //FOR THE WEBCAM
+        /*
+         * Instantiate an OpenCvCamera object for the camera we'll be using.
+         * Webcam stream goes to RC phone
+         */
+
+
+        /*
+         * Open the connection to the camera device. New in v1.4.0 is the ability
+         * to open the camera asynchronously which allows faster init time, and
+         * better behavior when pressing stop during init (i.e. less of a chance
+         * of tripping the stuck watchdog)
+         */
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+
+            @Override
+            public void onOpened() {
+                /*
+                 * Tell the webcam to start streaming images to us! Note that you must make sure
+                 * the resolution you specify is supported by the camera. If it is not, an exception
+                 * will be thrown.
+                 *
+                 * Keep in mind that the SDK's UVC driver (what OpenCvWebcam uses under the hood) only
+                 * supports streaming from the webcam in the uncompressed YUV image format. This means
+                 * that the maximum resolution you can stream at and still get up to 30FPS is 480p (640x480).
+                 * Streaming at e.g. 720p will limit you to up to 10FPS and so on and so forth.
+                 *
+                 * Also, we specify the rotation that the webcam is used in. This is so that the image
+                 * from the camera sensor can be rotated such that it is always displayed with the image upright.
+                 * For a front facing camera, rotation is defined assuming the user is looking at the screen.
+                 * For a rear facing camera or a webcam, rotation is defined assuming the camera is facing
+                 * away from the user.
+                 */
+                //320px x 340px
+                webcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+
+                /*
+                 * Specify the image processing pipeline we wish to invoke upon receipt
+                 * of a frame from the camera. Note that switching pipelines on-the-fly
+                 * (while a streaming session is in flight) *IS* supported.
+                 */
+
+                webcam.setPipeline(pipeline);
+
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("errorCode", errorCode);
+            }
+        });
+        // Tell telemetry to update faster than the default 250ms period :)
+        //telemetry.setMsTransmissionInterval(20);
+
+        // telemetry.addLine("Waiting for start");
+        // telemetry.update();
+
+        //Wait for the user to press start on the Driver Station
+
+        waitForStart();
+
+        //Manages Telemetry and stopping the stream
+        while (opModeIsActive()) {
+
+            sleep(2000);
+            telemetry.addData("Analysis", pipeline.getAnalysis());
+            telemetry.update();
+
+            switch (pipeline.getAnalysis()) {
+                case LEFT:
+                    Trajectory traj1 = drive.trajectoryBuilder(new Pose2d())
+                            .lineToLinearHeading(new Pose2d(-50,-11.5,Math.toRadians(180)))
+                            .build();
+                   Trajectory traj2 = drive.trajectoryBuilder(new Pose2d(-50,-11.5,Math.toRadians(180)))
+                           .lineToSplineHeading(new Pose2d(-53,-15,Math.toRadians(90)))
+                           .build();
+
+                   Trajectory traj3 = drive.trajectoryBuilder(new Pose2d(-53,-15,Math.toRadians(90)))
+                           .back(4.5)
+                           .build();
+
+                    Trajectory traj4 = drive.trajectoryBuilder(new Pose2d(-53,-19.5, Math.toRadians(90)))
+                                    .forward(12)
+                                            .build();
+
+                    drive.followTrajectory(traj1);
+                    wrist.setPosition(0.19);
+                    sleep(1500);
+                    clawR.setPosition(0.6);
+                    sleep(1000);
+                    wrist.setPosition(0.5);
+                    sleep(400);
+                    drive.followTrajectory(traj2);
+                    wrist.setPosition(0.35);
+                    sleep(300);
+                    clawR.setPosition(0.6);
+                    sleep(30);
+                    drive.followTrajectory(traj3);
+                    clawR.setPosition(0.6);
+                    sleep(500);
+                    clawR.setPosition(0.4);
+                    sleep(300);
+                    drive.followTrajectory(traj4);
+                    sleep(2000);
+
+
+
+
+
+
+                    break;
+                case CENTER:
+
+                    break;
+                case RIGHT:
+
+                    break;
+            }
+
+            //reminder to use the KNO3 auto transitioner once this code is working
+
+            webcam.stopStreaming();
+            webcam.closeCameraDevice();
+            break;
+        }
+    }
+}
